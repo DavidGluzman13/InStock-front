f2b804626ec7b139a888bc50a91da2de376011c0
Neha Dhaka <nehadhaka43@gmail.com>
2023-06-06 19:06:43 -0400

    fix routing issues

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -2,17 +2,15 @@ import InventoryAdd from "../../components/Inventory Add/InventoryAdd";
 import InventoryEdit from "../../components/Inventory Edit/InventoryEdit";
 import InventoryItemDetails from "../../components/Inventory Item Details/InventoryItemDetails";
 import InventoryList from "../../components/Inventory List/InventoryList";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function Inventory() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/inventories" element={<InventoryList />} />
-        <Route path="/inventories/add" element={<InventoryAdd />} />
-        <Route path="/inventories/edit" element={<InventoryEdit />} />
-        <Route path="/inventories/:id" element={<InventoryItemDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<InventoryList />} />
+      <Route path="/add" element={<InventoryAdd />} />
+      <Route path="/edit" element={<InventoryEdit />} />
+      <Route path="/:id" element={<InventoryItemDetails />} />
+    </Routes>
   );
 }
--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -3,20 +3,15 @@ import WarehouseAdd from "../../components/Warehouse Add/WarehouseAdd";
 import WarehouseInformation from "../../components/Warehouse Information/WarehouseInformation";
 import WarehouseList from "../../components/Warehouse List/WarehouseList";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function Warehouse() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/warehouses" />} />
-          <Route path="/warehouses" element={<WarehouseList />} />
-          <Route path="/warehouses/add" element={<WarehouseAdd />} />
-          <Route path="/warehouses/edit" element={<WarehouseEdit />} />
-          <Route path="/warehouses/:id" element={<WarehouseInformation />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Routes>
+      <Route path="/" element={<WarehouseList />} />
+      <Route path="/add" element={<WarehouseAdd />} />
+      <Route path="/edit" element={<WarehouseEdit />} />
+      <Route path="/:id" element={<WarehouseInformation />} />
+    </Routes>
   );
 }
