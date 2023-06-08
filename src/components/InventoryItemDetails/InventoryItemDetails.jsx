d4fdef53034bb5243003331e53643dc41e6ee515
gajeev <gajeev@gmail.com>
2023-06-08 10:34:08 -0400

    get item from inventory

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -1,3 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./InventoryItemDetails.scss";
+
 export default function InventoryItemDetails() {
-  return <h1>This is a InventoryItemDetails</h1>;
+  const url = "http://localhost:8080/api/inventories/";
+  const { id } = useParams();
+  const [inventoryItem, setInventoryItem] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url + id)
+      .then((response) => {
+        setInventoryItem(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  if (inventoryItem === null) {
+    return <div>Loading...</div>;
+  } else {
+    document.title = `InStock - Inventory Item Detail - ${inventoryItem.item_name}`;
+  }
+
+  return <h1>{inventoryItem.item_name}</h1>;
 }
