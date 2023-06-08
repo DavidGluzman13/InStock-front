3fa1de8aa27532e97b628c81388f54c5c6461b78
David Gluzman <david.m.gluzman@gmail.com>
2023-06-08 13:40:09 -0400

    adds all structure to WarehouseDetails.js and its styling elements, changes to _global.scss, creating a new SVG for back arrow

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,46 +1,68 @@
 import whiteEditIcon from "../../assets/Icons/edit-24px-white.svg";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
-import "./WarehouseDetails.scss"
+import "./WarehouseDetails.scss";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function WarehouseDetails({ currentWarehouse }) {
+  const navigate = useNavigate();
+
+  function navigateToHome(e) {
+    e.preventDefault();
+    navigate("/");
+  }
+
+  // fix the id you're inputing in the path
+  function navigateToEdit(e) {
+    e.preventDefault();
+    navigate(`/warehouses/${id}/edit`);
+  }
+
   return (
     <>
-      <div className="details-wrapper">
-        {/* --------------------Header----------------------- */}
-        <div className="heading">
-          <div className="heading__titlediv">
-            <img className="back-arrow" src={backArrow} alt="back-arrow" />
-            <h1>{currentWarehouse.warehouse_name}</h1>
-          </div>
-          <div className="heading__image">
-            <img
-              className="edit-icon"
-              src={whiteEditIcon}
-              alt="white edit icon"
-            />
-            <p className="edit-text">Edit</p>
-          </div>
-        </div>
-        {/* ------------------Details------------------ */}
-        <div className="details">
-          <div className="details__address">
-            <h4 className="address-header">WAREHOUSE ADDRESS:</h4>
-            <p className="address-text">{currentWarehouse.address}</p>
-          </div>
-          <div className="details__contact">
-            <div className="contact-person">
-              <h4>CONTACT NAME:</h4>
-              <span>{currentWarehouse.contact_name}</span>
-              <span>{currentWarehouse.contact_position}</span>
+      <div className="component-wrapper">
+        <div className="wh-component">
+          {/* --------------------Header----------------------- */}
+          <div className="heading">
+            <div className="heading__titlediv">
+              <button className="button" onClick={navigateToHome}>
+                <img className="back-arrow" src={backArrow} alt="back-arrow" />
+              </button>
+              <h1>{currentWarehouse.warehouse_name}</h1>
             </div>
-            <div className="contact-info">
-              <h4>CONTACT INFORMATION:</h4>
-              <span>{currentWarehouse.contact_phone}</span>
-              <span>{currentWarehouse.contact_email}</span>
+            <button className="heading__image button" onClick={navigateToEdit}>
+              <img
+                className="edit-icon"
+                src={whiteEditIcon}
+                alt="white edit icon"
+              />
+              <p className="edit-text">Edit</p>
+            </button>
+          </div>
+          {/* ------------------Details------------------ */}
+          <div className="wh-details">
+            <div className="wh-details__address">
+              <h4 className="address-header">WAREHOUSE ADDRESS:</h4>
+              <p className="address-text">
+                {currentWarehouse.address},{` `}
+                {currentWarehouse.city},{` `}
+                {currentWarehouse.country}
+              </p>
+            </div>
+            <div className="wh-details__contact">
+              <div className="contact-person">
+                <h4>CONTACT NAME:</h4>
+                <span>{currentWarehouse.contact_name}</span>
+                <span>{currentWarehouse.contact_position}</span>
+              </div>
+              <div className="contact-info">
+                <h4>CONTACT INFORMATION:</h4>
+                <span>{currentWarehouse.contact_phone}</span>
+                <span>{currentWarehouse.contact_email}</span>
+              </div>
             </div>
           </div>
+          {/* -----------------Items------------------ */}
         </div>
-        {/* -----------------Items------------------ */}
       </div>
     </>
   );
