eb5559ff3afef73aea3a5c3d3ef70ff1909b67e3
David Gluzman <david.m.gluzman@gmail.com>
2023-06-09 13:53:43 -0400

    adds Inventory add call for the front-end, styling (without buttons)

--- src/components/InventoryAdd/InventoryAdd.jsx
+++ src/components/InventoryAdd/InventoryAdd.jsx
@@ -1,3 +1,215 @@
+import "./InventoryAdd.scss";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import dropDownArrow from "../../assets/Icons/arrow_drop_down-24px.svg";
+
 export default function InventoryAdd() {
-  return <h1>This is a InventoryAdd</h1>;
+  const navigate = useNavigate();
+
+  const [item_name, setItem_name] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [status, setStatus] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [warehouse_id, setWarehouse_id] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newItem = {
+      item_name,
+      description,
+      category,
+      status,
+      quantity,
+      warehouse_id,
+    };
+
+    console.log(newItem);
+    axios
+      .post("http://localhost:8080/api/inventories/", newItem)
+      .then((response) => {
+        console.log(response.data);
+        alert("Added");
+        navigate("/inventories");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  function navigateToHome(e) {
+    e.preventDefault();
+    navigate(-1);
+  }
+
+  function alertCancel(e) {
+    e.preventDefault();
+    alert("Canceled");
+    navigate(-1);
+  }
+
+  const handleItem_name = (event) => {
+    setItem_name(event.target.value);
+  };
+  const handleDescription = (event) => {
+    setDescription(event.target.value);
+  };
+  const handleCategory = (event) => {
+    setCategory(event.target.value);
+  };
+  const handleQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+  const handleWarehouse_id = (event) => {
+    setWarehouse_id(event.target.value);
+  };
+
+  const handleRadioButton = (value) => {
+    setStatus(value === 1 ? "In Stock" : "Out of Stock");
+  };
+
+  return (
+    <>
+      <div className="component-wrapper">
+        <div className="add-component">
+          {/* --------------------Header----------------------- */}
+          <div className="itm-heading">
+            <button className="button" onClick={navigateToHome}>
+              <img className="back-arrow" src={backArrow} alt="back-arrow" />
+            </button>
+            <h1>Add New Inventory Item</h1>
+          </div>
+          <div className="add-item">
+            {/* -----------------form 1--------------- */}
+            <form
+              onSubmit={handleSubmit}
+              method="POST"
+              className="add-item__form"
+            >
+              <div className="item-detials">
+                <h2 className="item-detials__heading">Item Detials</h2>
+                <h3>Item Name</h3>
+                <input
+                  name="nameItem"
+                  className="input"
+                  placeholder="Add a name of item"
+                  onChange={handleItem_name}
+                  value={item_name}
+                />
+                <h3>Description</h3>
+                <input
+                  name="description"
+                  className="input--bigger"
+                  placeholder="Add item description"
+                  onChange={handleDescription}
+                  value={description}
+                />
+                <h3>category</h3>
+                <div className="category-wrapper">
+                  <select
+                    name="category"
+                    className="select"
+                    placeholder="Please select"
+                    onChange={handleCategory}
+                    value={category}
+                  >
+                    <option value="Accessories">Accessories</option>
+                    <option value="Apparel">Apparel</option>
+                    <option selected value="Electronics">
+                      Electronics
+                    </option>
+                    <option value="Gear">Gear</option>
+                    <option value="Health">Health</option>
+                  </select>
+                  <img
+                    className="drop-arrow"
+                    src={dropDownArrow}
+                    alt="drop down arrow"
+                  />
+                </div>
+              </div>
+              {/* -----------------form 2--------------- */}
+              <div className="item-availability">
+                <h2 className="item-availability__heading">
+                  Item Availability
+                </h2>
+                <div className="status">
+                  <h3>Status</h3>
+                  <div className="status-wrapper">
+                    <div className="status-wrapper__instock">
+                      <input
+                        className="radio-button"
+                        type="radio"
+                        checked={status === "In Stock"}
+                        onChange={() => handleRadioButton(1)}
+                      />
+                      <label>In Stock</label>
+                    </div>
+                    <div className="status-wrapper__out-of-stock">
+                      <input
+                        className="radio-button"
+                        type="radio"
+                        checked={status === "Out of Stock"}
+                        onChange={() => handleRadioButton(0)}
+                      />
+                      <label>Out of Stock</label>
+                    </div>
+                  </div>
+                </div>
+                <div className="quantity">
+                  <h3>Quantity</h3>
+                  <input
+                    name="quantity"
+                    className="input"
+                    placeholder="0"
+                    type="number"
+                    onChange={handleQuantity}
+                    value={quantity}
+                  />
+                </div>
+                <div className="warehouse">
+                  <h3>Warehouse</h3>
+                  <div className="warehouse_id-wrapper">
+                    <select
+                      name="warehouse_id"
+                      className="select"
+                      placeholder="Please select"
+                      onChange={handleWarehouse_id}
+                      value={warehouse_id}
+                    >
+                      <option value="1">Manhattan</option>
+                      <option value="2">Washington</option>
+                      <option selected value="3">
+                        Jersey
+                      </option>
+                      <option value="4">Santa Monica</option>
+                      <option value="5">Seattle</option>
+                      <option value="6">Miami</option>
+                      <option value="7">Boston</option>
+                    </select>
+                    <img
+                      className="drop-arrow"
+                      src={dropDownArrow}
+                      alt="drop down arrow"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="buttons">
+                <button onClick={alertCancel} className="button">
+                  Cancel
+                </button>
+                <button type="submit" className="add-button">
+                  + Add Item
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
