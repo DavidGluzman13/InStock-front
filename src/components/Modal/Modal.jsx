7f8851d64ed945a7f353c63cb768541ec68634b5
Neha Dhaka <nehadhaka43@gmail.com>
2023-06-10 09:08:36 -0400

    update modal to be able to display msg for both warehouse and inventory list

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import "./Modal.scss";
 
-const Modal = ({ title, children, onClose }) => {
+const Modal = ({ title, children, onClose, source }) => {
   return (
     <div className="modal__overlay">
       <div className="modal__content">
@@ -9,10 +9,15 @@ const Modal = ({ title, children, onClose }) => {
             X
           </button>
           <div className="modal__text">
-            <h1>{`Delete ${title} Warehouse?`}</h1>
+            <h1>{`Delete ${title} ${
+              source === "warehouse" ? "Warehouse" : "inventory item"
+            }Warehouse?`}</h1>
             <p className="modal__description">
-              Please confirm that you’d like to delete the {title} from the list
-              of warehouses. You won’t be able to undo this action.
+              Please confirm that you’d like to delete
+              {source === "warehouse" ? "the" : ""}
+              {title} from the{" "}
+              {source === "warehouse" ? "list of warehouses" : "inventory list"}
+              . You won’t be able to undo this action.
             </p>
           </div>
         </div>
